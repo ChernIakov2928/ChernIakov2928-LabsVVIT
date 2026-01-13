a5ff805eb46b632ee0fcc6b54f9266c4f91349de
ChernIakov2928 <nalkabbaklan@gmail.com>
2026-01-13 13:37:51 +0300

    Update ЛР4.py

--- ЛР4/ЛР4.py
+++ ЛР4/ЛР4.py
@@ -1,18 +1,29 @@
 #Задание 1
 import math
-def square_root(num):
-    return math.sqrt(num)
-num = int(input('Введите число:'))
-square_root(num)
+number = int(input("Введите число: "))
+sqrt_number = math.sqrt(number)
+print(f"квадратный корень из {number} равен {sqrt_number}")
 
 import datetime
-print('Текущие дата и время:', datetime.datetime.now())
+print("Текущие дата и время:", datetime.datetime.now())
 
 #Задание 2
 import my_module
-my_module.sum(2,4)
+a = float(input("Введите первое число: "))
+b = float(input("Введите второе число: "))
+
+result = my_module.sum(a,b)
+
+print(f"Сумма чисел: {result}")
+
 
 #Задание 3
 from pack import numbers, strings
-numbers.even(2)
-strings.read_file('example.txt', 'all')
+a = float(input("Введите первое число: "))
+b = float(input("Введите второе число: "))
+result1 = numbers.multiply(a, b)
+
+print(f"{a} * {b} = {result1}")
+
+text = input("Введите строку: ")
+print(f"Перевёрнутая строка: {strings.reverse_string(text)}")
