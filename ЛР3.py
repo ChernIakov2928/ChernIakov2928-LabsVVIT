e1bbad460c03c5875841b2a4ed785e0b4a48eeb6
ChernIakov2928 <nalkabbaklan@gmail.com>
2026-01-13 12:57:50 +0300

    Update ЛР3.py

--- ЛР3.py
+++ ЛР3.py
@@ -2,21 +2,25 @@
 def read_file(namefile, mode = 'all'):
     try:
         if mode == 'all':
-            with open(namefile, 'r',) as file:
+            with open(namefile, 'r') as file:
                 content = file.read()
                 return(content)
         elif mode == 'line':
-            with open(namefile, 'r',) as file:
-                for line in file:
-                    return(line)
+            with open(namefile, 'r') as file:
+                 strings = [line for line in file]
+                 return strings
         else:
             print("Неизвестный тип чтения")
     except FileNotFoundError:
         print("Ошибка: файл не найден")
 
-read_file('example.txt', 'all')
-read_file('example.txt', 'line')
+result = read_file('example.txt', 'line')
 
+if type(result) == list:
+    for line in result:
+        print(line)
+else:
+    print(result)
 #Задание 2
 def write_new_file(namefile, content):
     with open(namefile, 'w') as file:
@@ -28,7 +32,7 @@ def append_to_file(namefile, content):
         file.write(content)
         print("Текст добавлен в файл", namefile)
 
-user_text = input('Введите текст для запсии в новый файл:')
+user_text = input('Введите текст для записи в новый файл:')
 write_new_file('user_input.txt', user_text)
 
 user_add = input("Введите текст для добавления в существующий файл: ")
